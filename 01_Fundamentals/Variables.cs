48aad74c8e0cd018ad000fe31137ed4582164035
LaraCodesDev <lagoonstudios.dev@gmail.com>
2026-08-17 19:54:40 +0200

    Add variables fundamentals exercise

---
+++ 01_Fundamentals/Variables.cs
@@ -0,0 +1,15 @@
+static class Variables
+{
+    public static void Run()
+    {
+        string name = "Lara";
+        int age = 32;
+        double temperature = 22.5;
+        bool learningCSharp = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Temperature: " + temperature);
+        Console.WriteLine("Learning C#: " + learningCSharp);
+    }
+}
