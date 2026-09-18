bf48bdbd788de1e480743737564683372ce15117
LaraCodesDev <lagoonstudios.dev@gmail.com>
2026-09-18 19:37:42 +0200

    Sort photos into extension folders

--- 09_OOP/PhotoSorter.cs
+++ 09_OOP/PhotoSorter.cs
@@ -15,30 +15,37 @@ public class PhotoSorter
         {
             Directory.CreateDirectory(destinationFolder);
         }
-        string [] photos = Directory.GetFiles(sourceFolder);
+
+        string[] photos = Directory.GetFiles(sourceFolder);
 
         foreach (var photo in photos)
         {
             string extension = Path.GetExtension(photo).ToLower();
 
-            if (extension == ".jpg" || extension == ".png" ||extension == ".jpeg")
+            if (extension == ".jpg" || extension == ".png" || extension == ".jpeg")
             {
                 Console.WriteLine(Path.GetFileName(photo));
-                string destinationPath = Path.Combine(destinationFolder, Path.GetFileName(photo));
-                
+
+                string extensionFolder = Path.Combine(destinationFolder, extension);
+                Directory.CreateDirectory(extensionFolder);
+
+                string destinationPath = Path.Combine(
+                    extensionFolder,
+                    Path.GetFileName(photo)
+                );
+
                 if (File.Exists(destinationPath))
                 {
                     Console.WriteLine("Photo already exists.");
                 }
-                
                 else
                 {
                     File.Copy(photo, destinationPath);
-                    Console.WriteLine("Photo copied successfully.");    
+                    Console.WriteLine("Photo copied successfully.");
                 }
+
                 Console.WriteLine(destinationPath);
             }
-            
         }
     }
 }
